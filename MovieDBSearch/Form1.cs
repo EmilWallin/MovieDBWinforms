be9f901f31f27fee1a138aa6a203242e49e043a2
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2021-12-08 09:41:48 +0100

    Add project files.

---
+++ MovieDBSearch/Form1.cs
@@ -0,0 +1,10 @@
+namespace MovieDBSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MovieDBSearch/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDBSearch
+{
+    internal class Movie
+    {
+        public Genre[] Genres { get; set; }
+        public string Homepage { get; set; }
+        public string Original_Language { get; set; }
+        public string Overview { get; set; }
+        public string Poster_Path { get; set; }
+        public string Release_Date { get; set; }
+        public int Runtime { get; set; }
+        public string Title { get; set; }
+        public float Vote_Average { get; set; }
+    }
+}
